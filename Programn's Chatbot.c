9415567116f152be5f6eb18cd3a2a095288e88db
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-10-25 23:05:40 +0530

    Functions Required

--- Programn's Chatbot.c
+++ Programn's Chatbot.c
@@ -10,3 +10,13 @@ void quar(){
     printf("   Press 4 for  (No,Thanks)\n");
 }
 
+void prob(){
+              printf("Press 1 if App is not Responding properly:)\n");
+    printf("Press 2 if Provided Links Of Notes And Solutions is not Responding:)\n");
+}
+
+char over(char c[]){
+    char e='n';
+    c[1]=e;
+}
+
