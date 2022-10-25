13d3ae2f59583a675bd2e2bf5b40d56e9c345a85
Tanmay <96858809+TanmayPr7@users.noreply.github.com>
2022-10-25 23:05:00 +0530

    Qurries Display

---
+++ Programn's Chatbot.c
@@ -0,0 +1,12 @@
+# This is the basic chatbot program for this project
+
+#include <stdio.h>
+#include <string.h>
+
+void quar(){
+    printf("Press 1 for (Problem In using App)\n");
+    printf("    Press 2 for (Feedback)\n");
+    printf("   Press 3 for (Contact Us)\n");
+    printf("   Press 4 for  (No,Thanks)\n");
+}
+
